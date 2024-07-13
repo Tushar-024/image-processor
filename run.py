b9bbff2ad81ca98da4b45590f2500c2d98ffb1ec
Tusharsingla024 <tushar.tech@altswealth.com>
2024-07-13 23:25:09 +0530

    Health check route added

--- run.py
+++ run.py
@@ -2,5 +2,10 @@ from app import create_app
 
 app = create_app()
 
+
+@app.route('/')
+def health_check():
+    return 'Image processing service is up and running!'
+
 if __name__ == '__main__':
     app.run(port=5001,debug=True)
